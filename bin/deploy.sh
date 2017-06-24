7d83249ec43280b960ce6a9e40224486bc90b3f9
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-06-24 08:31:43 +0100

    build: update deploy script to force push

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,7 +1,9 @@
-cd _site
+git stash
+git branch -D gh-pages
 git checkout -b gh-pages
 git add -A
 git commit -m "chore: publish site"
-git push origin HEAD
+git push origin HEAD --force
 git checkout master
+git stash pop
 echo "Site deployed"
