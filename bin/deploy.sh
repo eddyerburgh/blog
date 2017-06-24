6c25d4e747cfe5098dfe39c195f07f8ddafb48a1
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-06-24 17:49:34 +0100

    refactor: remove deploy script

--- bin/deploy.sh
+++
@@ -1,9 +0,0 @@
-git stash
-git branch -D gh-pages
-git checkout -b gh-pages
-git add -A
-git commit -m "chore: publish site"
-git push origin HEAD --force
-git checkout master
-git stash pop
-echo "Site deployed"
