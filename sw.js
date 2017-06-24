bcaa0f395ada2629931f92d11e35f7e268af01f1
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-06-24 20:33:47 +0100

    perf: remove whitespace from sw.js

--- sw.js
+++ sw.js
@@ -5,36 +5,21 @@
 var urlsToCache = [];
 // Cache posts
 // Limits the number of posts that gets cached to 3
-// Reads a piece of front-matter in each post that directs the second loop to the folder where the assets are held
-{% for post in site.posts limit:3 %}
-urlsToCache.push("{{ post.url }}")
-{% for file in site.static_files %}
-{% if file.path contains post.assets %}
-urlsToCache.push("{{ file.path }}")
-{% endif %}
-{% endfor %}
-{% endfor %}
+{% for post in site.posts limit:3 -%}urlsToCache.push("{{ post.url }}");{%- endfor -%}
+
+// Cache static files
+{% for file in site.static_files -%}{%- if file.path contains post.assets -%}urlsToCache.push("{{ file.path }}");{%- endif -%}{%- endfor -%}
 
 // Cache pages
-// Do nothing if it's either an AMP page (as these are served via Googles cache) or the blog page
-// Fallback to the offline pages for these
-{% for page in site.html_pages %}
-{% if page.path contains 'amp-html' or page.path contains 'blog' %}
-{% else if %}
-urlsToCache.push("{{ page.url }}")
-{% endif %}
-{% endfor %}
+{% for page in site.html_pages -%}
+    urlsToCache.push("{{ page.url }}");
+{%- endfor -%}
 
 // Cache assets
-// Removed assets/posts because I only want assets from the most recent posts getting cached
-{% for file in site.static_files %}
-{% if file.extname == '.js' or file.path contains '/assets/images' %}
-urlsToCache.push("{{ file.path }}")
-{% endif %}
-{% endfor %}
+{% for file in site.static_files -%}{%- if file.extname == '.js' or file.path contains '/assets/images' -%}urlsToCache.push("{{ file.path }}");{%- endif -%}{%- endfor -%}
 
 // Cache name: adjust version number to invalidate service worker cachce.
-var CACHE_NAME = 'james-ives-cache-v2';
+var CACHE_NAME = 'edd-yerburgh-cache-v2';
 
 self.addEventListener('install', function(event) {
     // Perform install steps
